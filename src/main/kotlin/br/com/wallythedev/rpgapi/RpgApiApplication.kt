3672cafcd661c8deed537191ec9eaa4dde43d9e5
Lukaz Rocha <lukazrocha05@gmail.com>
2023-05-28 15:58:03 -0300

    :sparkles: Adding initial project configuration

---
+++ src/main/kotlin/br/com/wallythedev/rpgapi/RpgApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.wallythedev.rpgapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RpgApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RpgApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/wallythedev/rpgapi/RpgApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.wallythedev.rpgapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RpgApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
